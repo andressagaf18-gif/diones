9583096b64a2b07a358bd41e7ad0ae7aa417b8a0
andressagaf18-gif <andressa.gaf18@gmail.com>
2026-08-17 01:10:58 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Admin from "./Admin.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const caminho = window.location.pathname;
+
+const Componente =
+  caminho === "/admin" || caminho.startsWith("/admin/")
+    ? Admin
+    : App;
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <App />
+    <Componente />
   </React.StrictMode>
 );
